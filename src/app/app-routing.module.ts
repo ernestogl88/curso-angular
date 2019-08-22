4832a840e0085af86df50ea28e7dcced455fa01a
Bdeo <ernesto@bdeo.es>
2019-08-22 10:42:43 +0200

    Configuradas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FrutaComponente} from './fruta/fruta.component';
+import {EmpleadoComponente} from './empleado/empleado.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'empleados', component: EmpleadoComponente},
+  {path: 'frutas', component: FrutaComponente}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [EmpleadoComponente, FrutaComponente]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';   // hacer import del componente
-import { FrutaComponente } from './fruta/fruta.component';
-import { EmpleadoComponente } from './empleado/empleado.component';
+
 
 @NgModule({
   declarations: [ //aquí se cargan los componentes creados
     AppComponent,
-    FrutaComponente,
-    EmpleadoComponente
+    routingComponents
   ],
   imports: [
     BrowserModule,
