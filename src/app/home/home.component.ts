a982510d91a46635a4a81e48d329445c32832506
Bdeo <ernesto@bdeo.es>
2019-08-22 13:28:45 +0200

    Ejemplo de servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,13 +8,21 @@ import {RopaService} from '../services/ropa.service';
   providers: [RopaService]
 })
 export class HomeComponent implements OnInit {
-
+   public listado_ropa:Array<string>;
+   public prenda_a_guardar:string;
   constructor(
     private _ropaService: RopaService
   ) {}
 
   ngOnInit() {
-    console.log(this._ropaService.prueba('Prenda1'));
+    this.listado_ropa = this._ropaService.getColeccion();
+    console.log(this.listado_ropa);
   }
-
+  guardarPrenda(){
+    this._ropaService.addRopa(this.prenda_a_guardar);
+    this.prenda_a_guardar = null;
+  }
+  borrarPrenda(i:number){
+    this._ropaService.deleteRopa(i);
+  }
 }
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -4,7 +4,21 @@ import { Injectable } from '@angular/core';
 
 export class RopaService {
     public nombre_prenda:string;
+    public coleccion_ropa = ['Pantalón', 'Camiseta']
     prueba(nombre_prenda){
         return nombre_prenda;
+    }
+
+    addRopa(prenda:string):Array<string>{
+        this.coleccion_ropa.push(prenda);
+        return this.getColeccion();
+    }
+
+    getColeccion(){
+        return this.coleccion_ropa;
+    }
+
+    deleteRopa(index:number){
+        this.coleccion_ropa.splice(index,1);
     }
 }
